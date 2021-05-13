c1fd2528e7fc71faf4e5b3f50f107d4c6735b100
thoriqdharmawan <thoriqd21@gmail.com>
2021-05-13 21:27:55 +0700

    feat: initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {
+  StatusBar,
+  Image,
+  Animated,
+  View,
+  Dimensions,
+  StyleSheet,
+} from "react-native";
+import { DATA } from "./constant";
+const { width } = Dimensions.get("screen");
 
-export default function App() {
+const imageW = width * 0.7;
+const imageH = imageW * 1.54;
+
+export default () => {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "#000" }}>
+      <StatusBar hidden />
+      <View style={StyleSheet.absoluteFillObject}>
+        {DATA.map((image, index) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+            (index + 1) * width,
+          ];
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange: [0, 1, 0],
+          });
+
+          return (
+            <Animated.Image
+              key={`image-${index}`}
+              source={{ uri: image }}
+              style={[StyleSheet.absoluteFillObject, { opacity }]}
+              blurRadius={10}
+            />
+          );
+        })}
+      </View>
+      <Animated.FlatList
+        data={DATA}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                contentOffset: { x: scrollX },
+              },
+            },
+          ],
+          { useNativeDriver: true }
+        )}
+        keyExtractor={(_, index) => index.toString()}
+        horizontal
+        pagingEnabled
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                width,
+                justifyContent: "center",
+                alignItems: "center",
+                shadowColor: "#000",
+                shadowOpacity: 0.5,
+                shadowOffset: {
+                  width: 0,
+                  height: 0,
+                },
+                shadowRadius: 20,
+              }}
+            >
+              <Image
+                source={{ uri: item }}
+                style={{
+                  width: imageW,
+                  height: imageH,
+                  resizeMode: "cover",
+                  borderRadius: 16,
+                }}
+              />
+            </View>
+          );
+        }}
+      />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
